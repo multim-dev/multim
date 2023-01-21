52b7398bf05b49be08bfacea7d691826e9733c6f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-01-22 01:36:42 +0900

    test: テストするブラウザからsafariを削除

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,6 @@ kotlin {
                 useKarma {
                     useFirefox()
                     useChrome()
-                    useSafari()
                 }
             }
         }
