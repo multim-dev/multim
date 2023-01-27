b12908567ddca2a6fdb245baa69d8f6b7033f25f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-01-28 00:37:54 +0900

    feat: タイムラインの共通APIを追加

---
+++ core/src/main/kotlin/dev/usbharu/multim/api/TimelineApi.kt
@@ -0,0 +1,24 @@
+package dev.usbharu.multim.api
+
+import dev.usbharu.multim.model.Status
+import dev.usbharu.multim.model.Timeline
+
+interface TimelineApi {
+    suspend fun availableTimelines(): List<Timeline>
+
+    suspend fun onReceive(callback: (List<Status>) -> Unit)
+
+    // todo 返り値を詳細にする
+    suspend fun connect(timeline: Timeline): Boolean
+
+    /**
+     * Disconnect
+     *
+     * @param timeline 切断するタイムライン
+     * @param force 強制的に切断し、もし受信しても無視するように要求する。
+     * @return
+     */
+    suspend fun disconnect(timeline: Timeline, force: Boolean = false): Boolean
+
+
+}
---
+++ core/src/main/kotlin/dev/usbharu/multim/model/Timeline.kt
@@ -0,0 +1,4 @@
+package dev.usbharu.multim.model
+
+abstract class Timeline {
+}
