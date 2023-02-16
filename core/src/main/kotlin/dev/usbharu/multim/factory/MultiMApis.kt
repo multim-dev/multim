e23eccaef8c445bd55fdbf36b5a8574fea68a987
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-16 17:39:13 +0900

    feat: Api定義に絵文字APIを追加

--- core/src/main/kotlin/dev/usbharu/multim/factory/MultiMApis.kt
+++ core/src/main/kotlin/dev/usbharu/multim/factory/MultiMApis.kt
@@ -1,11 +1,13 @@
 package dev.usbharu.multim.factory
 
 import dev.usbharu.multim.api.AccountApi
+import dev.usbharu.multim.api.EmojiApi
 import dev.usbharu.multim.api.StatusApi
 import dev.usbharu.multim.api.TimelineApi
 
 abstract class MultiMApis(
     var statusApi: StatusApi,
     var accountApi: AccountApi,
-    var timelineApi: TimelineApi
+    var timelineApi: TimelineApi,
+    var emojiApi: EmojiApi
 )
