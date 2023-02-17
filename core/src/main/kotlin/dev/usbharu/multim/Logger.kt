8148f79aeb65cc80dba135ad7f478b12a02ab245
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-17 21:51:27 +0900

    feat: inlineを廃止、Error型に対応

--- core/src/main/kotlin/dev/usbharu/multim/Logger.kt
+++ core/src/main/kotlin/dev/usbharu/multim/Logger.kt
@@ -1,48 +1,94 @@
 package dev.usbharu.multim
 
+import dev.usbharu.multim.error.Error
+import dev.usbharu.multim.error.ThrowableError
 import io.github.aakira.napier.Napier
 
-@Suppress("NOTHING_TO_INLINE")
 object Logger {
 
     const val nullString = "null"
-    inline fun info(tag: String? = null, message: String? = null) =
+    fun info(tag: String? = null, message: String? = null) =
         Napier.i(message ?: nullString, null, tag)
 
-    inline fun info(message: String? = null) = Napier.i(message ?: nullString)
+    fun info(message: String? = null) = Napier.i(message ?: nullString)
 
-    inline fun info(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
+    fun info(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
         Napier.i(message ?: nullString, throwable, tag)
 
-    inline fun debug(tag: String? = null, message: String? = null) =
+    fun info(tag: String? = null, message: String? = null, error: Error) {
+        message?.let { Napier.i(it, null, tag) }
+        if (error is ThrowableError) {
+            Napier.i(error.message, error.throwable, tag)
+        } else {
+            Napier.i(error.message, null, tag)
+        }
+    }
+
+    fun debug(tag: String? = null, message: String? = null) =
         Napier.d(message ?: nullString, null, tag)
 
-    inline fun debug(message: String? = null) = Napier.d(message ?: nullString)
+    fun debug(message: String? = null) = Napier.d(message ?: nullString)
 
-    inline fun debug(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
+    fun debug(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
         Napier.d(message ?: nullString, throwable, tag)
 
-    inline fun error(tag: String? = null, message: String? = null) =
+    fun debug(tag: String? = null, message: String? = null, error: Error) {
+        message?.let { Napier.d(it, null, tag) }
+        if (error is ThrowableError) {
+            Napier.d(error.message, error.throwable, tag)
+        } else {
+            Napier.d(error.message, null, tag)
+        }
+    }
+
+    fun error(tag: String? = null, message: String? = null) =
         Napier.e(message ?: nullString, null, tag)
 
-    inline fun error(message: String? = null) = Napier.e(message ?: nullString)
+    fun error(message: String? = null) = Napier.e(message ?: nullString)
 
-    inline fun error(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
+    fun error(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
         Napier.e(message ?: nullString, throwable, tag)
 
-    inline fun warn(tag: String? = null, message: String? = null) =
+    fun error(tag: String? = null, message: String? = null, error: Error) {
+        message?.let { Napier.e(it, null, tag) }
+        if (error is ThrowableError) {
+            Napier.e(error.message, error.throwable, tag)
+        } else {
+            Napier.e(error.message, null, tag)
+        }
+    }
+
+    fun warn(tag: String? = null, message: String? = null) =
         Napier.w(message ?: nullString, null, tag)
 
-    inline fun warn(message: String? = null) = Napier.w(message ?: nullString)
+    fun warn(message: String? = null) = Napier.w(message ?: nullString)
 
-    inline fun warn(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
+    fun warn(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
         Napier.w(message ?: nullString, throwable, tag)
 
-    inline fun trace(tag: String? = null, message: String? = null) =
+    fun warn(tag: String? = null, message: String? = null, error: Error) {
+        message?.let { Napier.w(it, null, tag) }
+        if (error is ThrowableError) {
+            Napier.w(error.message, error.throwable, tag)
+        } else {
+            Napier.w(error.message, null, tag)
+        }
+    }
+
+    fun trace(tag: String? = null, message: String? = null) =
         Napier.v(message ?: nullString, null, tag)
 
-    inline fun trace(message: String? = null) = Napier.v(message ?: nullString)
+    fun trace(message: String? = null) = Napier.v(message ?: nullString)
 
-    inline fun trace(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
+    fun trace(tag: String? = null, message: String? = null, throwable: Throwable? = null) =
         Napier.v(message ?: nullString, throwable, tag)
+
+    fun trace(tag: String? = null, message: String? = null, error: Error) {
+        message?.let { Napier.v(it, null, tag) }
+        if (error is ThrowableError) {
+            Napier.v(error.message, error.throwable, tag)
+        } else {
+            Napier.v(error.message, null, tag)
+        }
+    }
 }
