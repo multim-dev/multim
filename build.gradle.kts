c0343be46587f8b6d6d3c19f97fe4c9f82c81256
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-17 19:14:52 +0900

    chore: 公開先にGitHubPackagesを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,10 +93,14 @@ subprojects {
 
     publishing{
         repositories{
-//            maven{
-//                name = "GitHubLocals"
-//                url = uri("file://")
-//            }
+            maven{
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/usbharu/multim")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
         }
         publications{
             register<MavenPublication>("gpr") {
