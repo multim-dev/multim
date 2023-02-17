41fe72c7a63e437a00ecb0228e0346375e814671
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-17 11:39:57 +0900

    feat: 未実装APIのオブジェクトを作成

--- core/src/main/kotlin/dev/usbharu/multim/api/EmojiApi.kt
+++ core/src/main/kotlin/dev/usbharu/multim/api/EmojiApi.kt
@@ -1,5 +1,8 @@
 package dev.usbharu.multim.api
 
+import com.github.michaelbull.result.Err
+import dev.usbharu.multim.error.ErrorType
+import dev.usbharu.multim.error.MultiMError
 import dev.usbharu.multim.error.MultiMResult
 import dev.usbharu.multim.model.Emoji
 
@@ -7,4 +10,14 @@ interface EmojiApi {
     suspend fun get(name:String):MultiMResult<Emoji>
 
     suspend fun findByName(name:String):MultiMResult<List<Emoji>>
+}
+
+object NotImplEmojiApi : EmojiApi {
+    override suspend fun get(name: String): MultiMResult<Emoji> {
+        return Err(MultiMError("emoji get not implements",null, ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun findByName(name: String): MultiMResult<List<Emoji>> {
+        return Err(MultiMError("emoji findByName not implements",null,ErrorType.NOT_IMPL))
+    }
 }
--- core/src/main/kotlin/dev/usbharu/multim/api/StatusApi.kt
+++ core/src/main/kotlin/dev/usbharu/multim/api/StatusApi.kt
@@ -1,6 +1,8 @@
 package dev.usbharu.multim.api
 
+import com.github.michaelbull.result.Err
 import com.github.michaelbull.result.Result
+import dev.usbharu.multim.error.ErrorType
 import dev.usbharu.multim.error.MultiMError
 import dev.usbharu.multim.model.*
 
@@ -27,4 +29,62 @@ interface StatusApi {
     suspend fun addToBookmarks(id: StatusId): Result<Unit,MultiMError>
     suspend fun removeFromBookmarks(id: StatusId): Result<Unit,MultiMError>
     suspend fun getPreviousAndNext(id: StatusId): Result<PreviousAndNextPosts,MultiMError>
+}
+
+
+object NotImplStatusApi : StatusApi {
+    override suspend fun post(status: StatusForPost): Result<Status, MultiMError> {
+        return Err(MultiMError("post not implements",null, ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun delete(id: StatusId): Result<Unit, MultiMError> {
+        return Err(MultiMError("delete not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun findById(id: StatusId): Result<Status, MultiMError> {
+        return Err(MultiMError("findById not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun addReaction(id: StatusId, reaction: Reaction): Result<Unit, MultiMError> {
+        return Err(MultiMError("addReaction not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun removeReaction(
+        id: StatusId,
+        reaction: Reaction?
+    ): Result<Unit, MultiMError> {
+        return Err(MultiMError("removeReaction not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun reactions(id: StatusId): Result<Map<Reaction, Int>, MultiMError> {
+        return Err(MultiMError("reactions not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun replies(id: StatusId): Result<List<Status>, MultiMError> {
+        return Err(MultiMError("replies not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun repost(id: StatusId): Result<Status, MultiMError> {
+        return Err(MultiMError("repost not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun unRepost(id: StatusId): Result<Unit, MultiMError> {
+        return Err(MultiMError("unRepost not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun replyTo(id: StatusId, status: StatusForPost): Result<Status, MultiMError> {
+        return Err(MultiMError("replyTo not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun addToBookmarks(id: StatusId): Result<Unit, MultiMError> {
+        return Err(MultiMError("addToBookmarks not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun removeFromBookmarks(id: StatusId): Result<Unit, MultiMError> {
+        return Err(MultiMError("removeFromBookmarks not implements",null,ErrorType.NOT_IMPL))
+    }
+
+    override suspend fun getPreviousAndNext(id: StatusId): Result<PreviousAndNextPosts, MultiMError> {
+        return Err(MultiMError("getPreviousAndNext not implements",null,ErrorType.NOT_IMPL))
+    }
 }
