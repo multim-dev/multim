48ebe47d9a7abf8d6f917b964a45c1f723c58c84
usbharu <64310155+usbharu@users.noreply.github.com>
2023-02-27 12:09:15 +0900

    feat: 無効化したとき、無効化されるように

--- core/src/main/kotlin/dev/usbharu/multim/cache/InMemoryLRUCache.kt
+++ core/src/main/kotlin/dev/usbharu/multim/cache/InMemoryLRUCache.kt
@@ -1,10 +1,9 @@
 package dev.usbharu.multim.cache
 
 import io.github.reactivecircus.cache4k.Cache
-import kotlin.ClassCastException
 
-class InMemoryLRUCache(maxSize:Long) : CacheableApi {
-    private val cache = Cache.Builder().maximumCacheSize(maxSize).build<String,Any>()
+class InMemoryLRUCache(maxSize: Long) : CacheableApi {
+    private val cache = Cache.Builder().maximumCacheSize(maxSize).build<String, Any>()
     private var enable: Boolean = true
     override fun purge() {
         cache.invalidateAll()
@@ -19,16 +18,21 @@ class InMemoryLRUCache(maxSize:Long) : CacheableApi {
     }
 
     override fun <T : Any> cache(method: String, key: String, value: T): T {
-        cache.put(method+key, value)
+        if (enable) {
+            cache.put(method + key, value)
+        }
         return value
     }
 
     override fun <T> get(method: String, key: String): T? {
-        val any = cache.get(method + key)
-        return try {
-            any as T?
-        }catch (e:ClassCastException){
-            null
+        if (enable) {
+            val any = cache.get(method + key)
+            return try {
+                any as T?
+            } catch (e: ClassCastException) {
+                null
+            }
         }
+        return null
     }
 }
